8137ebbf51962262db0b78c7e4f787bd9e8d7060
Thomas Wilburn <news@thomaswilburn.net>
2016-10-05 17:04:07 -0700

    So much more content.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,34 @@
-// require("./lib/social");
+require("./lib/social");
 // require("./lib/ads");
 // var track = require("./lib/tracking");
 
-require("component-responsive-frame/child");
+var $ = require("./lib/qsa");
+var debounce = require("./lib/debounce");
+
+var photos = $(".photo-embed");
+var years = $(".year-label");
+
+photos.forEach(p => p.classList.add("hidden"));
+var reflow = document.body.offsetHeight;
+photos.forEach(p => p.classList.add("animate"));
+
+window.addEventListener("scroll", debounce(function() {
+  photos = photos.filter(function(p) {
+    var bounds = p.getBoundingClientRect();
+    if (bounds.top < window.innerHeight * .8) {
+      p.classList.remove("hidden");
+      return false;
+    }
+    return true;
+  });
+  var current = document.querySelector(".year-label.activated");
+  if (current) current.classList.remove("activated");
+  for (var i = years.length - 1; i >= 0; i--) {
+    var label = years[i];
+    var bounds = label.getBoundingClientRect();
+    if (bounds.top > 0 && bounds.top < window.innerHeight * .5) {
+      label.classList.add("activated");
+      break;
+    }
+  }
+}));
